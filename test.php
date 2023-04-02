442d8e874cd4a6e8912ec6aeb2c1aaec4589627e
Fisnik Hazrolli <71229274+fisnikhz@users.noreply.github.com>
2023-04-02 22:38:47 +0200

    fixing testpage

--- test.php
+++ test.php
@@ -11,21 +11,20 @@
 			<div class="col-lg-6 mx-auto">
 				<div class="card">
 					<div class="card-header">
-						<h5 class="card-title text-center">Send a Message</h5>
+						<h5 class="card-title text-center">Send a Message via Twilio</h5>
 					</div>
 					<div class="card-body">
 						<form method="post">
-  <div class="form-group">
-    <label for="to">To:</label>
-    <input type="text" class="form-control" name="to" id="to" placeholder="Enter receiver's phone number">
-  </div>
-  <div class="form-group">
-    <label for="message">Message:</label>
-    <textarea class="form-control" name="message" id="message" placeholder="Enter your message"></textarea>
-  </div>
-  <button type="submit" class="btn btn-primary">Send</button>
-</form>
-
+						  <div class="form-group">
+						    <label for="to">To:</label>
+						    <input type="text" class="form-control" name="to" id="to" placeholder="Enter receiver's phone number">
+						  </div>
+						  <div class="form-group">
+						    <label for="message">Message:</label>
+						    <textarea class="form-control" name="message" id="message" placeholder="Enter your message"></textarea>
+						  </div>
+						  <button type="submit" class="btn btn-primary">Send</button>
+						</form>
 					</div>
 				</div>
 			</div>
@@ -44,29 +43,39 @@ use Twilio\Rest\Client;
 // Set your Twilio phone number
 $twilio_number = '+15855221230';
 
-// Get the recipient's phone number from the form input
-$to_number = $_POST['to'];
+// Check if form has been submitted and both to and message are present
+if (isset($_POST['to']) && isset($_POST['message'])) {
 
-// Get the message from the form input
-$message = $_POST['message'];
+    // Get the recipient's phone number from the form input
+    $to_number = $_POST['to'];
 
-// Create a new Twilio client
-$client = new Client($account_sid, $auth_token);
+    // Get the message from the form input
+    $message = $_POST['message'];
 
-try {
-    // Use the Twilio API to send an SMS message
-    $message = $client->messages->create(
-        $to_number,
-        array(
-            'from' => $twilio_number,
-            'body' => $message
-        )
-    );
+    // Create a new Twilio client
+    $client = new Client($account_sid, $auth_token);
 
-    // Print a success message if the SMS was sent successfully
-    echo "Message sent successfully to " . $to_number . " (Message ID: " . $message->sid . ")";
-} catch (Exception $e) {
-    // Print an error message if there was an error sending the SMS
-    echo "Error sending SMS: " . $e->getMessage();
+    try {
+        // Use the Twilio API to send an SMS message
+        $message = $client->messages->create(
+            $to_number,
+            array(
+                'from' => $twilio_number,
+                'body' => $message
+            )
+        );
+
+        // Print a success message if the SMS was sent successfully
+        echo "Message sent successfully to " . $to_number . " (Message ID: " . $message->sid . ")";
+    } catch (Exception $e) {
+        // Print an error message if there was an error sending the SMS
+        echo "Error sending SMS: " . $e->getMessage();
+    }
 }
+
+
 ?>
+
+</body>
+</html>
+
