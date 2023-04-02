04a6d3084097cfce23b4d4b9fc3c9d92a532c7ca
BinakuEndri <binakuendri@gmail.com>
2023-04-02 15:16:30 +0200

    Hierarchy of files

---
+++ view/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/index.css">
+    <title>Login</title>
+  </head>
+
+  <body>
+    <div class="container">
+      <img src="../images/pr-logo.webp" alt="" width="150px">
+      <h2 style="text-align:center;">Universiteti i Prishtinës</h2>
+      <h3>"Hasan Prishtina"</h3>
+      <form action="" method="" id="login-form">
+        <div class="form-field">
+          <label for="user-name">
+            Username or Email
+          </label>
+          <input type="text" name="user-name" id="user-name" size="50" required autofocus />
+        </div>
+
+        <div class="form-field">
+          <label for="pass">
+            Password
+          </label>
+          <input type="password" name="password" id="pass" size="50" required />
+        </div>
+
+        <div id="form-submit">
+          <input type="submit" value="Login" />
+        </div>
+        <div id="forgot-link">
+          <a href="">Forgot Password</a>
+        </div>
+      </form>
+
+
+    </div>
+
+  </body>
+
+</html>
